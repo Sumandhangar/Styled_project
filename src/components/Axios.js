ac1523abb9733d49ade52d4d24d12f66e09be0c5
sumandhangar <sumandhangar@mp2it.com>
2022-03-10 17:48:46 +0530

    created checkbox in form and fetched,display checkbox

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -26,22 +26,16 @@ const Axios = (props) => {
             });
     }, []);
     // ***************************For Delete ********************************
-    
-    
-          
-               const onDelete = (id) => {
-            axios.delete(`https://62289f859fd6174ca82a068c.mockapi.io/database/${id}`)
-               .then((id) => {
-                   if (id.status === 200 && window.confirm) {
-                       alert("Student successfully deleted");
-                       window.location.reload();
-                   } else window.confirm("Error")
-               })
-               .catch((err) => alert("Something went wrong"));   
-       }
-    
-           
-
+    const onDelete = (id) => {
+        axios.delete(`https://62289f859fd6174ca82a068c.mockapi.io/database/${id}`)
+            .then((id) => {
+                if (id.status === 200 && window.confirm) {
+                    alert("Student successfully deleted");
+                    window.location.reload();
+                } else window.confirm("Error")
+            })
+            .catch((err) => alert("Something went wrong"));
+    }
     // *****************************for Create new user********************
     const toggleform = () => {
         setCreate(!create)
