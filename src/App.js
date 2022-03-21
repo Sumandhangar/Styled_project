7996820ac7ec9dff3a180b69b6d9be5fa6ea6289
sumandhangar <sumandhangar@mp2it.com>
2022-03-21 13:13:45 +0530

    changed  folder name

--- src/App.js
+++ src/App.js
@@ -17,38 +17,22 @@ import { GlobalStyle } from "./components/GlobalStyle";
 // import Navigation from "./components/Tests/Navigation";
 // import Form from "./components/Tests/Form";
 // ***************************************************
-// import './components/web/Style'
-// import Navbar from "./components/web/Navbar";
-// import Banner from "./components/web/Banner";
-// import About from "./components/web/About";
-// import Work from "./components/web/Work";
-// import Product from "./components/web/Product";
-// import Contact from "./components/web/Contact";
-// import Footer from "./components/web/Footer";
-// ************************************************
-import Navbar from "./components/webcopy/Navbar";
-import Banner from "./components/webcopy/Banner";
-import About from "./components/webcopy/About";
-import Work from "./components/webcopy/Work";
-import Json from "./components/webcopy/Json";
-import Product from "./components/webcopy/Product";
-import Contact from "./components/webcopy/Contact";
-import Footers from "./components/webcopy/Footer";
-import { Rays } from "../src/components/webcopy/Css";
+
+import Navbar from "./components/Styled_project/Navbar";
+import Banner from "./components/Styled_project/Banner";
+import About from "./components/Styled_project/About";
+import Work from "./components/Styled_project/Work";
+import Json from "./components/Styled_project/Json";
+import Product from "./components/Styled_project/Product";
+import Contact from "./components/Styled_project/Contact";
+import Footers from "./components/Styled_project/Footer";
+import { Rays } from "../src/components/Styled_project/Css";
 // **********************************************
 function App() {
 
   return (
     <>
-      {/* <div className="container-fluid rays"><Navbar /><Banner /></div>
-      <div className="container-fluid">
-        <section id="about"><About /></section>
-        <section id="work"><Work /></section>
-        <section id="product"><Product /></section>
-        <section id="contact"><Contact /></section>
-        <Footer />
-      </div> */}
-      {/* ********************************************************** */}
+
 
       <Rays className="container-fluid"><Json /><Banner /></Rays>
       <div className="container">
